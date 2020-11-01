0480b18f48a1cc6c8ece82cfb4bccfaeb502ad66
Conor Bury <conordebru@gmail.com>
2020-11-01 01:55:24 +0000

    Added map to description age and began working on layout for this final page. CB

--- app/src/main/java/com/ccbury/joblisting/GradJobActivity.java
+++ app/src/main/java/com/ccbury/joblisting/GradJobActivity.java
@@ -6,12 +6,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class GradJobActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class GradJobActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grad_job);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
         Button homeBack = findViewById(R.id.home1);
         homeBack.setOnClickListener(new View.OnClickListener() {
@@ -20,5 +31,17 @@ public class GradJobActivity extends AppCompatActivity {
                 startActivity(activity2Intent);
             }
         });
+        
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng NCI = new LatLng(53.34, -6.24);
+        googleMap.addMarker(new MarkerOptions()
+                .position(NCI)
+                .title("Marker for NCI Dublin"));
+        float zoomLevel = 16.0f; //This goes up to 21
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(NCI, zoomLevel));
+    }
+
 }
